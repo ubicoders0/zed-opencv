7bf2af95623bf75977a7c0ac4a8b00b71d8415c6
root <anon@ubicoders.com>
2024-08-12 00:07:00 +0000

    updated

--- python/zed-opencv.py
+++ python/zed-opencv.py
@@ -2,6 +2,22 @@ import sys
 import numpy as np
 import pyzed.sl as sl
 import cv2
+import time
+
+class TimerTicTok:
+    def __init__(self):
+        self.prev = time.perf_counter()
+        self.dt = 0.0
+
+    def update(self):
+        now = time.perf_counter()
+        self.dt = now - self.prev
+        self.prev = now
+
+    def pprint(self):
+        # print up to 2 decimal places
+        print(f"dt = {self.dt:.2f} sec, freq = {1.0 / self.dt:.2f} Hz")
+
 
 help_string = "[s] Save side by side image [d] Save Depth, [n] Change Depth format, [p] Save Point Cloud, [m] Change Point Cloud format, [q] Quit"
 prefix_point_cloud = "Cloud_"
@@ -119,7 +135,8 @@ def main() :
     if len(sys.argv) >= 2 :
         input_type.set_from_svo_file(sys.argv[1])
     init = sl.InitParameters(input_t=input_type)
-    init.camera_resolution = sl.RESOLUTION.HD1080
+    init.camera_resolution = sl.RESOLUTION.HD720
+    init.camera_fps = 60
     init.depth_mode = sl.DEPTH_MODE.PERFORMANCE
     init.coordinate_units = sl.UNIT.MILLIMETER
 
@@ -147,9 +164,13 @@ def main() :
     point_cloud = sl.Mat()
 
     key = ' '
+    timer = TimerTicTok()
     while key != 113 :
         err = zed.grab(runtime)
         if err == sl.ERROR_CODE.SUCCESS :
+            timer.update()
+            timer.pprint()
+
             # Retrieve the left image, depth image in the half-resolution
             zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, image_size)
             zed.retrieve_image(depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, image_size)
